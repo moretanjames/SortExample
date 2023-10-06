1265f2e31e643ab74c2dcbc8ff1063fe255337d5
Tanner Harding <moretanjames@gmail.com>
2023-10-06 11:37:44 -0500

    vertical alignment

--- app/src/main/java/tjd/yt/sort/recylcerview/LazyColumnImpl.kt
+++ app/src/main/java/tjd/yt/sort/recylcerview/LazyColumnImpl.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -27,7 +28,10 @@ fun LazyColumnImpl(modifier: Modifier = Modifier) {
 @Composable
 fun PersonImpl(person: Person) {
   Surface {
-    Row(modifier = Modifier.padding(16.dp)) {
+    Row(
+      verticalAlignment = Alignment.CenterVertically,
+      modifier = Modifier.padding(16.dp)
+    ) {
       Column(modifier = Modifier.weight(1f)) {
         Text(text = person.name)
         Text(text = person.birthDayString)
@@ -36,7 +40,7 @@ fun PersonImpl(person: Person) {
       Box(
         modifier = Modifier
           .size(24.dp)
-          .background(person.faveColor, shape = RoundedCornerShape(4.dp))
+          .background(color = person.faveColor, shape = RoundedCornerShape(4.dp))
       )
     }
   }
